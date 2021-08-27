01488b0d1e4e7005f7813ad979809c32837f4eae
Ben Anderson <benanderson.david@gmail.com>
2021-08-27 02:35:48 +0100

    Added method to get health icon for GUI

--- core/src/main/java/com/terraboxstudios/instantreplay/versionspecific/item/ItemFactory.java
+++ core/src/main/java/com/terraboxstudios/instantreplay/versionspecific/item/ItemFactory.java
@@ -6,4 +6,6 @@ public interface ItemFactory {
 
     ItemStack getEmptyItemGUIPlaceholder();
 
+    ItemStack getHealthItemGUI();
+
 }
--- v1_8_R1/src/main/java/com/terraboxstudios/instantreplay/verionspecific/v1_8_R1/item/ItemFactoryImpl.java
+++ v1_8_R1/src/main/java/com/terraboxstudios/instantreplay/verionspecific/v1_8_R1/item/ItemFactoryImpl.java
@@ -3,17 +3,17 @@ package com.terraboxstudios.instantreplay.verionspecific.v1_8_R1.item;
 import com.terraboxstudios.instantreplay.versionspecific.item.ItemFactory;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemFactoryImpl implements ItemFactory {
 
     @Override
     public ItemStack getEmptyItemGUIPlaceholder() {
-        ItemStack item = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
-        ItemMeta itemMeta = item.getItemMeta();
-        itemMeta.setDisplayName(" ");
-        item.setItemMeta(itemMeta);
-        return item;
+        return new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
+    }
+
+    @Override
+    public ItemStack getHealthItemGUI() {
+        return new ItemStack(Material.SPECKLED_MELON, 1);
     }
 
 }
