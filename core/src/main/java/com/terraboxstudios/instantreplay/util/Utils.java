065bdead2d6723836aef7ed78320ae1c18052ab8
Ben Anderson <benanderson.david@gmail.com>
2021-08-31 21:13:10 +0100

    Added method to send timestamp message to player who is in a replay, will copy the timestamp to clipboard when message clicked.

--- core/src/main/java/com/terraboxstudios/instantreplay/util/Utils.java
+++ core/src/main/java/com/terraboxstudios/instantreplay/util/Utils.java
@@ -1,10 +1,13 @@
 package com.terraboxstudios.instantreplay.util;
 
 import com.terraboxstudios.instantreplay.InstantReplay;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
-import java.util.*;
+import java.util.Objects;
 
 public class Utils {
 
@@ -20,15 +23,21 @@ public class Utils {
 	}
 
 	public static String locationToString(Location loc) {
-		return Objects.requireNonNull(loc.getWorld()).getName()+":"+loc.getBlockX()+":"+loc.getBlockY()+":"+loc.getBlockZ()+":"+loc.getYaw()+":"+loc.getPitch();
+		return Objects.requireNonNull(loc.getWorld()).getName() + ":" + loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ() + ":" + loc.getYaw() + ":" + loc.getPitch();
 	}
 
 	public static String preciseLocationToString(Location loc) {
-		return Objects.requireNonNull(loc.getWorld()).getName()+":"+loc.getX()+":"+loc.getY()+":"+loc.getZ()+":"+loc.getYaw()+":"+loc.getPitch();
+		return Objects.requireNonNull(loc.getWorld()).getName() + ":" + loc.getX() + ":" + loc.getY() + ":" + loc.getZ() + ":" + loc.getYaw() + ":" + loc.getPitch();
 	}
 
 	public static String getReplayPrefix() {
 		return Config.getConfig().getBoolean("settings.use-plugin-prefix") ? Config.readColouredString("plugin-prefix") : "";
+	}
+
+	public static void sendTimestampMessage(Player player, long timestamp) {
+		TextComponent component = new TextComponent(TextComponent.fromLegacyText(Utils.getReplayPrefix() + Config.readColouredString("replay-timestamp-output").replace("{TIMESTAMP}", timestamp + "")));
+		component.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, timestamp + ""));
+		player.spigot().sendMessage(component);
 	}
 
 	public static void runOnMainThread(Runnable runnable) {
