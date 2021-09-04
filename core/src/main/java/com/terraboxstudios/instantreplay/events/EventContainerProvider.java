c076aa04ea7af2032fa8e33ad898c4df99c2df41
Ben Anderson <benanderson.david@gmail.com>
2021-09-04 17:37:50 +0100

    Added provide method which rounds the timestamps in superclass instead of subclass

--- core/src/main/java/com/terraboxstudios/instantreplay/events/EventContainerProvider.java
+++ core/src/main/java/com/terraboxstudios/instantreplay/events/EventContainerProvider.java
@@ -1,11 +1,20 @@
 package com.terraboxstudios.instantreplay.events;
 
-import org.bukkit.Location;
+import com.terraboxstudios.instantreplay.replay.ReplayContext;
+import com.terraboxstudios.instantreplay.util.Utils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface EventContainerProvider<T extends EventContainer> {
 
-    List<T> getEventContainers(Location replayLocation, int radius, long timestamp);
+    List<T> getEventContainers(ReplayContext context);
+
+    default List<T> provide(ReplayContext context) {
+        return getEventContainers(context)
+                .stream()
+                .peek(container -> container.setTime(Utils.roundTime(container.getTime())))
+                .collect(Collectors.toList());
+    }
 
 }
