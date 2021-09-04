a13bdf491b57a633435eb155806aee9df1c0cd20
Ben Anderson <benanderson.david@gmail.com>
2021-09-04 22:14:51 +0100

    Added version-specific implementation of click-able timestamp message. And using lombok now for getters

--- v1_8_R1/src/main/java/com/terraboxstudios/instantreplay/versionspecific/v1_8_R1/VersionSpecificProviderImpl.java
+++ v1_8_R1/src/main/java/com/terraboxstudios/instantreplay/versionspecific/v1_8_R1/VersionSpecificProviderImpl.java
@@ -3,27 +3,23 @@ package com.terraboxstudios.instantreplay.versionspecific.v1_8_R1;
 import com.terraboxstudios.instantreplay.versionspecific.VersionSpecificProvider;
 import com.terraboxstudios.instantreplay.versionspecific.item.ItemFactory;
 import com.terraboxstudios.instantreplay.versionspecific.npc.NPCFactory;
+import com.terraboxstudios.instantreplay.versionspecific.util.UtilsHelper;
 import com.terraboxstudios.instantreplay.versionspecific.v1_8_R1.item.ItemFactoryImpl;
 import com.terraboxstudios.instantreplay.versionspecific.v1_8_R1.npc.NPCFactoryImpl;
+import com.terraboxstudios.instantreplay.versionspecific.v1_8_R1.util.UtilsHelperImpl;
+import lombok.Getter;
 
+@Getter
 public class VersionSpecificProviderImpl implements VersionSpecificProvider {
 
     private final NPCFactory npcFactory;
     private final ItemFactory itemFactory;
+    private final UtilsHelper utilsHelper;
 
     public VersionSpecificProviderImpl() {
         npcFactory = new NPCFactoryImpl();
         itemFactory = new ItemFactoryImpl();
-    }
-
-    @Override
-    public NPCFactory getNPCFactory() {
-        return npcFactory;
-    }
-
-    @Override
-    public ItemFactory getItemFactory() {
-        return itemFactory;
+        utilsHelper = new UtilsHelperImpl();
     }
 
 }
