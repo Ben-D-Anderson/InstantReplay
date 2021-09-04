8b36546abddfb5cd35f2c082a670457772e933f4
Ben Anderson <benanderson.david@gmail.com>
2021-09-04 22:14:31 +0100

    Added version-specific implementation of click-able timestamp message

---
+++ core/src/main/java/com/terraboxstudios/instantreplay/versionspecific/util/UtilsHelper.java
@@ -0,0 +1,9 @@
+package com.terraboxstudios.instantreplay.versionspecific.util;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+
+public interface UtilsHelper {
+
+    ClickEvent getTimestampMessageClickEvent(long timestamp);
+
+}
