0985d621650bc808279a3d3e789424d389f0800b
Ben Anderson <benanderson.david@gmail.com>
2021-09-09 19:06:22 +0100

    Simplified parsing methods and added rounding to locations at 2 dp

--- core/src/main/java/com/terraboxstudios/instantreplay/util/Utils.java
+++ core/src/main/java/com/terraboxstudios/instantreplay/util/Utils.java
@@ -14,20 +14,29 @@ public class Utils {
 	public static Location stringToLocation(String str) {
 		String[] strArr = str.split(":");
 		Location loc = new Location(Bukkit.getServer().getWorld(strArr[0]), 0, 0, 0);
-		loc.setX(Double.parseDouble(strArr[1]));
-		loc.setY(Double.parseDouble(strArr[2]));
-		loc.setZ(Double.parseDouble(strArr[3]));
-		loc.setYaw(Float.parseFloat(strArr[4]));
-		loc.setPitch(Float.parseFloat(strArr[5]));
+		loc.setX(Integer.parseInt(strArr[1]));
+		loc.setY(Integer.parseInt(strArr[2]));
+		loc.setZ(Integer.parseInt(strArr[3]));
+		return loc;
+	}
+
+	public static Location stringToPreciseLocation(String str) {
+		String[] strArr = str.split(":");
+		Location loc = new Location(Bukkit.getServer().getWorld(strArr[0]), 0, 0, 0);
+		loc.setX(Utils.roundTwoDP(Double.parseDouble(strArr[1])));
+		loc.setY(Utils.roundTwoDP(Double.parseDouble(strArr[2])));
+		loc.setZ(Utils.roundTwoDP(Double.parseDouble(strArr[3])));
+		loc.setYaw(Utils.roundTwoDP(Float.parseFloat(strArr[4])));
+		loc.setPitch(Utils.roundTwoDP(Float.parseFloat(strArr[5])));
 		return loc;
 	}
 
 	public static String locationToString(Location loc) {
-		return Objects.requireNonNull(loc.getWorld()).getName() + ":" + loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ() + ":" + loc.getYaw() + ":" + loc.getPitch();
+		return Objects.requireNonNull(loc.getWorld()).getName() + ":" + loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ();
 	}
 
 	public static String preciseLocationToString(Location loc) {
-		return Objects.requireNonNull(loc.getWorld()).getName() + ":" + loc.getX() + ":" + loc.getY() + ":" + loc.getZ() + ":" + loc.getYaw() + ":" + loc.getPitch();
+		return Objects.requireNonNull(loc.getWorld()).getName() + ":" + Utils.roundTwoDP(loc.getX()) + ":" + Utils.roundTwoDP(loc.getY()) + ":" + Utils.roundTwoDP(loc.getZ()) + ":" + Utils.roundTwoDP(loc.getYaw()) + ":" + Utils.roundTwoDP(loc.getPitch());
 	}
 
 	public static String getReplayPrefix() {
@@ -60,10 +69,14 @@ public class Utils {
 		return (locationOne.getBlockX() >= locationTwo.getBlockX() - radius && locationOne.getBlockX() <= locationTwo.getBlockX() + radius)
 				&& (locationOne.getBlockZ() >= locationTwo.getBlockZ() - radius && locationOne.getBlockZ() <= locationTwo.getBlockZ() + radius)
 				&& locationOne.getWorld().getName().equals(locationTwo.getWorld().getName());
-
 	}
 
 	public static long roundTime(long time) {
 		return Math.round(time / (double) 100) * 100L;
 	}
+
+	public static long roundTwoDP(double value) {
+		return Math.round(value / (double) 100) * 100L;
+	}
+
 }
