fcc6994b31d932500fbc14a2fc847496df381624
Ben Anderson <benanderson.david@gmail.com>
2021-10-13 13:35:07 +0100

    Added a v1_8_R3 implementation

---
+++ v1_8_R3/src/main/java/com/terraboxstudios/instantreplay/versionspecific/v1_8_R3/item/ItemFactoryImpl.java
@@ -0,0 +1,25 @@
+package com.terraboxstudios.instantreplay.versionspecific.v1_8_R3.item;
+
+import com.terraboxstudios.instantreplay.versionspecific.item.ItemFactory;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+public class ItemFactoryImpl implements ItemFactory {
+
+    @Override
+    public ItemStack[] getHands(PlayerInventory playerInventory) {
+        return new ItemStack[]{playerInventory.getItemInHand(), null};
+    }
+
+    @Override
+    public ItemStack getEmptyItemGUIPlaceholder() {
+        return new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
+    }
+
+    @Override
+    public ItemStack getHealthItemGUI() {
+        return new ItemStack(Material.SPECKLED_MELON, 1);
+    }
+
+}
