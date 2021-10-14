bacd623a334012301a51a1b4b45b21d35025c577
Ben Anderson <benanderson.david@gmail.com>
2021-10-14 21:29:17 +0100

    Added v1_9_R2 implementation

---
+++ v1_9_R2/src/main/java/com/terraboxstudios/instantreplay/versionspecific/v1_9_R2/blocks/BlockChangeDataRaw.java
@@ -0,0 +1,14 @@
+package com.terraboxstudios.instantreplay.versionspecific.v1_9_R2.blocks;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.Material;
+
+@Getter
+@AllArgsConstructor
+public class BlockChangeDataRaw {
+
+    private final Material material;
+    private final byte data;
+
+}
---
+++ v1_9_R2/src/main/java/com/terraboxstudios/instantreplay/versionspecific/v1_9_R2/npc/NPCSkinImpl.java
@@ -0,0 +1,12 @@
+package com.terraboxstudios.instantreplay.versionspecific.v1_9_R2.npc;
+
+import com.mojang.authlib.GameProfile;
+import com.terraboxstudios.instantreplay.versionspecific.npc.NPCSkin;
+
+public class NPCSkinImpl extends NPCSkin<GameProfile> {
+
+    public NPCSkinImpl(GameProfile skin) {
+        super(skin);
+    }
+
+}
