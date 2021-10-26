a5ab93af5e87ce4ea3b55649de9ccc347f14b6f2
Ben Anderson <benanderson.david@gmail.com>
2021-10-26 21:07:26 +0100

    Added data file

--- core/src/main/java/com/terraboxstudios/instantreplay/util/Config.java
+++ core/src/main/java/com/terraboxstudios/instantreplay/util/Config.java
@@ -6,14 +6,15 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Config {
 
-    private static FileConfiguration config;
-    private static File configFile;
+    private static FileConfiguration config, data;
+    private static File configFile, dataFile;
 
     private static final InstantReplay plugin = InstantReplay.getPlugin(InstantReplay.class);
 
@@ -26,14 +27,40 @@ public class Config {
         plugin.getConfig().options().copyDefaults(true);
         plugin.saveDefaultConfig();
         configFile = new File(plugin.getDataFolder(), "config.yml");
+
+        dataFile = new File(plugin.getDataFolder(), "data.yml");
+        if (!dataFile.exists()) {
+            try {
+                dataFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        data = YamlConfiguration.loadConfiguration(dataFile);
     }
 
     public static FileConfiguration getConfig() {
         return config;
     }
 
+    public static FileConfiguration getDataConfig() {
+        return data;
+    }
+
     public static void reloadConfig() {
         config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public static void reloadDataConfig() {
+        data = YamlConfiguration.loadConfiguration(dataFile);
+    }
+
+    public static void saveDataConfig() {
+        try {
+            Config.getDataConfig().save(dataFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static String readColouredString(String path) {
